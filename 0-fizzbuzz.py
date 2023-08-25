d323c812b064f710f4cd1d472331d847ae52ad58
KhileM <sakhile.motha.254@gmail.com>
2023-08-25 02:31:34 -0700

    Multiples of 15 printing FizzBuzz and not Buzz

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -3,6 +3,7 @@
 """
 import sys
 
+
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
@@ -16,7 +17,7 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:  # Check for multiples of both 3 and 5 first
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
@@ -25,6 +26,7 @@ def fizzbuzz(n):
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
+
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -35,4 +37,3 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
-
